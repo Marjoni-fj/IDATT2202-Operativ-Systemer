c59e83855dbae72a2427d4db805b922bc6cded9e
Fredrik <marjonifredrik@gmail.com>
2026-08-30 09:02:21 +0200

    perf[oving1]: replace scanf with fgets to accomodate for longer strings with spaces

--- ovinger/oving1/printx.c
+++ ovinger/oving1/printx.c
@@ -6,11 +6,11 @@ int main(int argc, char *argv[])
   char str[100];
   int count = (argc > 1) ? atoi(argv[1]) : 1;
   printf("Enter a string: ");
-  scanf("%99s", str);
+  fgets(str, sizeof(str), stdin);
 
   for (int i = 0; i < count; i++)
   {
-    printf("%s\n", str);
+    printf("%s", str);
   }
 
   return 0;
